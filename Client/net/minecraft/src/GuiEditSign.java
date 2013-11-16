8e0983ceff8adf691912a2e7aa47424cb276c296
vadik777qwe <vadik777qwe>
2013-11-16 09:18:29 +0400

    Fix bug unlimited char on sign

--- Client/net/minecraft/src/GuiEditSign.java
+++ Client/net/minecraft/src/GuiEditSign.java
@@ -86,13 +86,12 @@ public class GuiEditSign extends GuiScreen {
 	 */
 	protected void keyTyped(char par1, int par2) {
 		if (par2 == 200) {
-		
 
 			editLine = editLine - 1 & 3;
 		}
 
 		if (par2 == 208 || par2 == 28) {
-			
+
 			editLine = editLine + 1 & 3;
 		}
 
@@ -100,13 +99,12 @@ public class GuiEditSign extends GuiScreen {
 			entitySign.signText[editLine] = entitySign.signText[editLine]
 					.substring(0, entitySign.signText[editLine].length() - 1);
 		}
-       
-		if (CheckChar.a(par1) || entitySign.signText[editLine]
-				.length() >= 15) {
-			
+
+		if (CheckChar.a(par1) && entitySign.signText[editLine].length() <= 15) {
+
 			entitySign.signText[editLine] += par1;
 		}
-	
+
 	}
 
 	/**
